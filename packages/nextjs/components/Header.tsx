2488d841e976a3c94aa2bda5b7b3be58bafa3faf
DavNej <david.nejar@gmail.com>
2024-04-07 14:31:04 +0200

    edit header

--- packages/nextjs/components/Header.tsx
+++ packages/nextjs/components/Header.tsx
@@ -54,8 +54,8 @@ export const Header = () => {
             <Image alt="SE2 logo" className="cursor-pointer" fill src="/logo.svg" />
           </div>
           <div className="flex flex-col">
-            <span className="font-bold leading-tight">Team Solahonor</span>
-            <span className="text-xs">Solana Attestation Services</span>
+            <span className="font-bold leading-tight">{"Sol'Attest"}</span>
+            <span className="text-xs">Team Solahonor</span>
           </div>
         </Link>
       </div>
